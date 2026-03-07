5b1206f2d245bc75e71fc36e34c3cbe3b3256912
Andrew Lou <andrewloupro@gmail.com>
2026-03-06 22:37:43 -0800

    Added pipeline, requirements, traffic light traci

---
+++ README.md
@@ -0,0 +1,9 @@
+## Pipeline will go:
+1. SUMO runs simulation step
+2. TraCI fetches information/data every x seconds using built-in functions
+    - Takes data from each intersection
+    - Formats for multiple agents
+3. Data is fed into multi-agent system (PettingZoo, stable-baselines3, sumo-rl)
+4. RL loop:
+    - Error calculation
+5. Repeat
--- runner.py
+++ runner.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import time
+import traci
 
+'''
 from gymenv import SumoIntersectionEnv
 
 from stable_baselines3 import DQN
@@ -11,21 +13,52 @@ env = SumoIntersectionEnv()
 model = DQN("MlpPolicy", env, verbose=1, buffer_size=10000)
 
 model.learn(total_timesteps=100000)
+'''
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
-    sys.exit("'SUMO_HOME' is not declared")
+    sys.exit("Error: 'SUMO_HOME' env variable is not declared")
 
-import traci
 
 def run_simulation():
-    # checks if there are still vehicles left
+    step = 0
+
     while traci.simulation.getMinExpectedNumber() > 0:
 
+        # runs one tick of the simulation
         traci.simulationStep()
-        time.sleep(0.1)
+
+        # collects data every 5 steps
+        if step % 5 == 0:
+
+            # counts cars waiting at a red light
+            vehicles = traci.vehicle.getIDList()
+            waiting_cars = 0
+
+            for v in vehicles:
+                if traci.vehicle.getWaitingTime(v) > 0:
+                    waiting_cars += 1
+            
+            print(f"Time {step}: Waiting cars = {waiting_cars}")
+
+
+            # fetches traffic light states
+            tl_ids = traci.trafficlight.getIDList()
+
+            if len(tl_ids) > 0:
+
+                for i in range(len(tl_ids)):
+
+                    target_light = tl_ids[i]
+                    current_phase = traci.trafficlight.getPhase(target_light)
+
+                    print(f"Time {step}: Traffic Light '{target_light}' is in phase {current_phase}")
+
+
+        step += 1
+
 
         # add custom logic
     
@@ -35,9 +68,9 @@ def run_simulation():
 
 
 if __name__ == "__main__":
-    sumoBinary = "sumo-gui"
-    sumoCmd = [sumoBinary, "-c", "simulation.sumocfg.txt"]
+    sumoCmd = ["sumo-gui", "-c", "sim.sumocfg"]
 
     traci.start(sumoCmd)
+    print("Successfully connected to SUMO")
 
     run_simulation()
