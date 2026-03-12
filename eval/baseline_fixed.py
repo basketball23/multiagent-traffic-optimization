27de43b55d7dcee0526a78db6d56a6f26b0936ab
Andrew Lou <andrewloupro@gmail.com>
2026-03-11 22:54:34 -0700

    Fixed pedestrian calculations

--- eval/baseline_fixed.py
+++ eval/baseline_fixed.py
@@ -10,8 +10,10 @@ def get_intersection_metrics(tl_id):
 
     veh_wait_count = 0
     veh_wait_time = 0
+    veh_avg_wait_time = 0
     ped_wait_count = 0
     ped_wait_time = 0
+    ped_avg_wait_time = 0
 
     # Get unique lanes controlled by this traffic light
     lanes = set(traci.trafficlight.getControlledLanes(tl_id))
@@ -21,14 +23,17 @@ def get_intersection_metrics(tl_id):
         
         # Check if the lane is for pedestrians
         if "_w" in edge_id:
-            ped_wait_count += traci.edge.getLastStepHaltingNumber(edge_id)
-            ped_wait_time += traci.edge.getWaitingTime(edge_id)
+            pedestrian_ids = traci.edge.getLastStepPersonIDs(edge_id)
+            ped_wait_count += len(pedestrian_ids)
+
+            for p_id in pedestrian_ids:
+                ped_wait_time += traci.person.getWaitingTime(p_id)
         else:
             # Otherwise, it is a vehicle lane
             veh_wait_count += traci.lane.getLastStepHaltingNumber(lane)
             veh_wait_time += traci.lane.getWaitingTime(lane)
             
-    return veh_wait_count, veh_wait_time, ped_wait_count, ped_wait_time
+    return veh_wait_count, veh_wait_time, veh_avg_wait_time, ped_wait_count, ped_wait_time, ped_avg_wait_time
 
 def run_fixed_timer():
     """
@@ -42,7 +47,10 @@ def run_fixed_timer():
     
     with open('fixed_timer_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['step', 'vehicle_total_stopped', 'vehicle_total_waiting_time', 'pedestrian_total_stopped', 'pedestrian_total_waiting_time'])
+        writer.writerow(['step', 'vehicle_total_stopped', 
+                         'vehicle_total_waiting_time', 'vehicle_average_waiting_time', 
+                         'pedestrian_total_stopped', 'pedestrian_total_waiting_time', 
+                         'pedestrian_average_waiting_time'])
         
         while step < 3600:
             traci.simulationStep()
@@ -57,7 +65,7 @@ def run_fixed_timer():
                 
 
                 for tl_id in tl_ids:
-                    v_count, v_time, p_count, p_time = get_intersection_metrics(tl_id)
+                    v_count, v_time, v_avg, p_count, p_time, p_avg = get_intersection_metrics(tl_id)
                     net_veh_count += v_count
                     net_veh_time += v_time
                     net_ped_count += p_count
--- eval/baseline_rule_based.py
+++ eval/baseline_rule_based.py
@@ -17,9 +17,12 @@ def get_intersection_metrics(tl_id):
     
     for lane in lanes:
         edge_id = traci.lane.getEdgeID(lane)
-        if "crosswalk" in edge_id or "walking" in edge_id:
-            ped_wait_count += traci.edge.getLastStepHaltingNumber(edge_id)
-            ped_wait_time += traci.edge.getWaitingTime(edge_id)
+        if "_w" in edge_id:
+            pedestrian_ids = traci.edge.getLastStepPersonIDs(edge_id)
+            ped_wait_count += len(pedestrian_ids)
+
+            for p_id in pedestrian_ids:
+                ped_wait_time += traci.person.getWaitingTime(p_id)
         else:
             veh_wait_count += traci.lane.getLastStepHaltingNumber(lane)
             veh_wait_time += traci.lane.getWaitingTime(lane)
@@ -27,11 +30,10 @@ def get_intersection_metrics(tl_id):
     return veh_wait_count, veh_wait_time, ped_wait_count, ped_wait_time
 
 def run_multi_rule_based():
-    # Set to "sumo" instead of "sumo-gui" for much faster data generation
     sumoCmd = ["sumo", "-c", "simulation/sim.sumocfg"]
     traci.start(sumoCmd)
     
-    print("Running Rule-Based Control and logging data...")
+    print("Running Dynamic Rule-Based Control and logging data...")
     
     step = 0
     phase_timers = {} 
@@ -44,22 +46,27 @@ def run_multi_rule_based():
             traci.simulationStep()
             tl_ids = traci.trafficlight.getIDList()
             
+            # --- 1. Dynamic Traffic Light Logic ---
             for target_light in tl_ids:
                 if target_light not in phase_timers:
                     phase_timers[target_light] = 0
+                    # FIX: Hijack the timer so SUMO doesn't auto-change it
+                    traci.trafficlight.setPhaseDuration(target_light, 1000)
                     
                 phase_timers[target_light] += 1
                 current_phase = traci.trafficlight.getPhase(target_light)
                 
-                if current_phase == 0 or current_phase == 2:
+                # Even phases (0, 2) are Green
+                if current_phase % 2 == 0: 
                     if phase_timers[target_light] >= MIN_GREEN_TIME:
                         waiting_entities = 0
+                        
+                        # FIX: Dynamic Queue Checking (looks for any red light)
+                        state_string = traci.trafficlight.getRedYellowGreenState(target_light)
                         lanes = traci.trafficlight.getControlledLanes(target_light)
                         
-                        for lane in lanes:
-                            if current_phase == 0 and ("E2" in lane or "W2" in lane): 
-                                waiting_entities += traci.lane.getLastStepHaltingNumber(lane)
-                            elif current_phase == 2 and ("N2" in lane or "S2" in lane):
+                        for i, lane in enumerate(lanes):
+                            if state_string[i].lower() == 'r':
                                 waiting_entities += traci.lane.getLastStepHaltingNumber(lane)
                         
                         if waiting_entities >= QUEUE_THRESHOLD:
@@ -67,19 +74,20 @@ def run_multi_rule_based():
                             traci.trafficlight.setPhase(target_light, next_phase)
                             phase_timers[target_light] = 0 
                 
-                elif current_phase == 1 or current_phase == 3:
-                    phase_timers[target_light] = 0
-
+                else:
+                    if phase_timers[target_light] >= 4: # Run yellow for exactly 4 seconds
+                        next_phase = (current_phase + 1) % 4
+                        traci.trafficlight.setPhase(target_light, next_phase)
+                        # Lock the new green light from auto-changing
+                        traci.trafficlight.setPhaseDuration(target_light, 1000)
+                        phase_timers[target_light] = 0
 
             if step % 5 == 0:
-                tl_ids = traci.trafficlight.getIDList()
-                
                 net_veh_count = 0
                 net_veh_time = 0
                 net_ped_count = 0
                 net_ped_time = 0
                 
-
                 for tl_id in tl_ids:
                     v_count, v_time, p_count, p_time = get_intersection_metrics(tl_id)
                     net_veh_count += v_count
@@ -88,7 +96,6 @@ def run_multi_rule_based():
                     net_ped_time += p_time
                     
                 writer.writerow([step, net_veh_count, net_veh_time, net_ped_count, net_ped_time])
-                
                 file.flush()
 
             step += 1
