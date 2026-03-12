e446044efbf4e227d2c3c6ee6d3d1ed42b0378ab
Andrew Lou <andrewloupro@gmail.com>
2026-03-12 16:24:20 -0700

    Evaluation script for RL model

--- evaluate.py
+++ evaluate.py
@@ -2,8 +2,11 @@ import sumo_rl
 import supersuit as ss
 
 from stable_baselines3 import PPO
-from utils import NeighborObservation, fair_wait_time_reward
+from stable_baselines3.common.vec_env import VecNormalize
+from utils import NeighborAwareObservation, fair_wait_time_reward, get_intersection_metrics
 
+import traci
+import csv
 
 def main():
     # creating multi-agent environment
@@ -12,9 +15,9 @@ def main():
         route_file='simulation/vehs.rou.xml,simulation/peds.rou.xml',
         out_csv_name='evaluation',
         use_gui=True,
-        num_seconds=20000,
+        num_seconds=3600,
         reward_fn=fair_wait_time_reward,
-        observation_class=NeighborObservation,
+        observation_class=NeighborAwareObservation,
     )
 
     # used to make compatible
@@ -26,19 +29,61 @@ def main():
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(env, num_vec_envs=1, num_cpus=1, base_class='stable_baselines3')
 
-    model = PPO.load("models1/ppo_model_5600000_steps.zip")
+    # TODO: Load saved env
+    stats_path = "training/models8/vec_normalize_3000000_steps.pkl" 
+    env = VecNormalize.load(stats_path, env)
+    env.training = False
+    env.norm_reward = False
 
-    obs = env.reset()
+    model = PPO.load("training/models8/ppo_model_3000000_steps.zip")
 
-    dones = [False]
-    while not all(dones):
-        # deterministic to stop taking random actions
-        actions, _states = model.predict(obs, deterministic=True)
+    with open('rl_model_data.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            'step', 'vehicle_total_stopped', 'vehicle_total_waiting_time',
+            'vehicle_average_waiting_time', 'pedestrian_total_stopped',
+            'pedestrian_total_waiting_time', 'pedestrian_average_waiting_time'
+        ])
 
-        obs, rewards, dones, infos = env.step(actions)
+        obs = env.reset()
+        done = False
+        
+        sim_time = 0 
 
+        while not done:
+            # rl agent chooses actions
+            action, _states = model.predict(obs, deterministic=True)
+            
+            obs, rewards, dones, infos = env.step(action)
+            sim_time += 5 
+            
+            done = dones.any()
+
+            tl_ids = traci.trafficlight.getIDList()
+            net_veh_count = 0
+            net_veh_time = 0
+            net_ped_count = 0
+            net_ped_time = 0
+
+            for tl_id in tl_ids:
+                v_count, v_time, p_count, p_time = get_intersection_metrics(tl_id)
+                net_veh_count += v_count
+                net_veh_time += v_time
+                net_ped_count += p_count
+                net_ped_time += p_time
+
+            veh_avg_wait = net_veh_time / net_veh_count if net_veh_count > 0 else 0
+            ped_avg_wait = net_ped_time / net_ped_count if net_ped_count > 0 else 0
+
+            # Write to CSV
+            writer.writerow([
+                sim_time, net_veh_count, net_veh_time, veh_avg_wait,
+                net_ped_count, net_ped_time, ped_avg_wait
+            ])
+            file.flush()
+
+    print("Evaluation finished! Data saved to rl_model_data.csv")
     env.close()
-
 
 if __name__ == "__main__":
     main()
